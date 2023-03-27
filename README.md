1699f16b17a9bce675fa76869cddcd4d0940d164
Kristian Nymann Jakobsen <kristian@nymann.dev>
2023-03-27 19:57:52 +0200

    :tada: Initialize template from nymann/python-template

---
+++ README.md
@@ -0,0 +1,5 @@
+# Dk Analyzer Api
+
+## Development
+
+For help getting started developing check [DEVELOPMENT.md](DEVELOPMENT.md)
---
+++ src/dk_analyzer_api/asgi.py
@@ -0,0 +1,7 @@
+from dk_analyzer_api.api import Api
+from dk_analyzer_api.core.config import Config
+from dk_analyzer_api.core.service_container import ServiceContainer
+
+config = Config()
+service_container = ServiceContainer(config=config)
+api = Api(config=config, service_container=service_container).api
