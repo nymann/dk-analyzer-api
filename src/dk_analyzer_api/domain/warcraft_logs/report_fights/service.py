5525f160ab1ae181365ad5af772ceafb32cf1203
Kristian Jakobsen <kristian@nymann.dev>
2024-07-27 23:08:21 +0200

    Add url to not found response

--- src/dk_analyzer_api/domain/warcraft_logs/report_fights/service.py
+++ src/dk_analyzer_api/domain/warcraft_logs/report_fights/service.py
@@ -1,3 +1,5 @@
+import logging
+
 from dk_analyzer_api.core.exceptions import NotFound
 from dk_analyzer_api.domain.warcraft_logs.api import WarcraftLogsApi
 from dk_analyzer_api.domain.warcraft_logs.report_fights.model import Report
@@ -9,7 +11,7 @@ class WarcraftLogsReportFightsService(WarcraftLogsApi):
             fight_id = self._get_fight_id(url)
             report_id = self._get_report_id(url)
         except Exception:
-            raise NotFound("Report or fight not found")
+            raise NotFound(f"Report or fight not found ('{url}')")
         if fight_id == "last":
             return self._get_last_report(report_id=report_id)
         return Report(report_id=report_id, fight_id=fight_id)
@@ -19,12 +21,14 @@ class WarcraftLogsReportFightsService(WarcraftLogsApi):
         # https://www.warcraftlogs.com/reports/MyvF2p1m7Df4VLjH#fight=25
         s = url.split("reports/")[1]
         b = s.split("#flight=")[0]
+        logging.info(b)
         return b.replace("/", "")
 
     def _get_fight_id(self, url: str) -> int:
         # https://www.warcraftlogs.com/reports/MyvF2p1m7Df4VLjH/#fight=25
         # https://www.warcraftlogs.com/reports/MyvF2p1m7Df4VLjH#fight=25
         s = url.split("#fight=")[1]
+        logging.info(s)
         return int(s)
 
     def _get_last_report(self, report_id: str) -> Report:
